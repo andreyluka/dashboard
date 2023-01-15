518c176ad63595b57a8824ae135775679ee50c44
andrey <andreiluka@ya.ru>
2023-01-15 11:02:40 +0300

    first commit

---
+++ src/components/appHeader.vue
@@ -0,0 +1,20 @@
+<script>
+
+</script>
+
+<template>
+  <header>
+    <div class="header__logo">
+      <img alt="logo" class="header__logo-img" src="@/assets/logo.svg"/>
+    </div>
+    
+  </header>
+</template>
+
+<style scoped lang="scss">
+.header__logo {
+  width: 50px;
+  height: 50px;
+}
+
+</style>
